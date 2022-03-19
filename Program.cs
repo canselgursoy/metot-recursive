2763ff9c8099d56c4725f953a2e2ecbed55c1b4d
DESKTOP-66BILQH\canse <canselgrsoy@gmail.com>
2022-03-19 19:20:52 +0300

    main.txt oluşturuldu

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace metotlar_extension_recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //rekürsif -öz yinelemeli
+            //3 üssü 4 =3*3*3*3
+
+            int result =1;
+            for (int i = 1; i < 5; i++)
+            {
+                result=result*3;
+            
+            }
+            Console.WriteLine(result);
+            Islemler instance = new Islemler();
+            Console.WriteLine(instance.Expo(3,4));
+        }
+
+    }
+    public class Islemler
+    {
+        public int Expo(int sayi,int üs)
+        {
+            if(üs<2)
+               return sayi;
+            return Expo(sayi,üs-1)*sayi;
+
+        }
+    }
+}
+
+
